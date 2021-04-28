1b6f8b537b1f6df80e3afe58e9f09067df8f11c5
Samuel Davila <samuelrdavila@gmail.com>
2021-04-28 14:42:10 -0500

    created folder and added first 2 solutions

---
+++ edabit/very_easy.py
@@ -0,0 +1,14 @@
+# Very Easy 
+
+# Create a function that takes two numbers as arguments and return their sum.
+
+def addition(a, b):
+	return a + b
+
+# Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+def addition(num):
+	return num + 1
+
+# 
+
