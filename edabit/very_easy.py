12391eedce331966846e42a6357d8f0947d8ed4a
Samuel Davila <samuelrdavila@gmail.com>
2021-04-28 14:56:08 -0500

    added more solutions

--- edabit/very_easy.py
+++ edabit/very_easy.py
@@ -10,5 +10,54 @@ def addition(a, b):
 def addition(num):
 	return num + 1
 
+# Write a function that takes an integer minutes and converts it to seconds.
+
+def convert(minutes):
+	return minutes * 60
+
+# Write a function that takes the base and height of a triangle and return its area.
+
+def tri_area(base, height):
+	return (base * height) / 2
+
+# Write a function that converts hours into seconds.
+
+def how_many_seconds(hours):
+	return hours * 60 * 60
+
+# Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+def next_edge(side1, side2):
+	return side1 + side2 - 1
+
+# There is a single operator in Python, capable of providing the remainder of a division operation. Two numbers are passed as parameters. 
+# The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+
+def remainder(x, y):
+	return x % y
+
+# Create a function that takes a string and returns it as an integer.
+
+def string_int(txt):
+	return int(txt)
+
+# Create a function that takes the age in years and returns the age in days.
+
+def calc_age(age):
+	return age * 365
+
+# Create a function that takes length and width and finds the perimeter of a rectangle.
+
+def find_perimeter(length, width):
+	return length * 2 + width * 2
+
+# Create a function that takes the age in years and returns the age in days.
+
+def calc_age(age):
+	return age * 365
+
 # 
+
+
+
 
