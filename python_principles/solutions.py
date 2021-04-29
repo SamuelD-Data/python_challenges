85b52da9e33c4722affe2a996bb165bc79a43962
Samuel Davila <samuelrdavila@gmail.com>
2021-04-29 16:56:18 -0500

    completed all exercises

--- python_principles/solutions.py
+++ python_principles/solutions.py
@@ -234,3 +234,25 @@ def validate(s):
         return True
     
 """
+Define a function named list_xor. Your function should take three parameters: n, list1 and list2. Your function must return whether n is exclusively in list1 or list2. In other words, if n is in both lists or in none of the lists, return False. If n is in only one of the lists, return True.
+"""
+
+def list_xor(n, list1, list2):
+    if (n in list1 and n in list2) or (n not in list1 and n not in list2):
+        return False
+    else:
+        return True
+    
+"""
+Define a function param_count that takes a variable number of parameters. The function should return the number of arguments it was called with.
+"""
+
+def param_count(*arg):
+    return len(arg)
+
+"""
+Write a function named format_number that takes a non-negative number as its only parameter. Your function should convert the number to a string and add commas as a thousands separator.
+"""
+
+def format_number(num):
+    return str("{:,}".format(num))
